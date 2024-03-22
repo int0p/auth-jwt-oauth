b4c97326ec25772fedec7e412538e836d9df1545
int0p <closer.fz@gmail.com>
2024-03-22 21:07:05 +0900

    validator

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
-
+use utoipa::ToSchema;
+use validator::Validate;
 
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize, sqlx::FromRow, Serialize, Clone)]
@@ -19,15 +20,38 @@ pub struct User {
     pub updated_at: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize, ToSchema)]
 pub struct RegisterUserSchema {
+    #[validate(length(min = 1, message = "Name is required"))]
     pub name: String,
+    #[validate(
+        length(min = 1, message = "Email is required"),
+        email(message = "Email is invalid")
+    )]
     pub email: String,
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
     pub password: String,
+    #[validate(
+        length(min = 1, message = "Please confirm your password"),
+        must_match(other = "password", message = "Passwords do not match")
+    )]
+    #[serde(rename = "passwordConfirm")]
+    pub password_confirm: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize, ToSchema)]
 pub struct LoginUserSchema {
+    #[validate(
+        length(min = 1, message = "Email is required"),
+        email(message = "Email is invalid")
+    )]
     pub email: String,
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
     pub password: String,
 }
